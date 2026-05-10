1284ad914aadb6645bb1b2bb500e2d5af244974c
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-05-10 19:59:47 -0400

    add another app

--- make-appimage.sh
+++ make-appimage.sh
@@ -12,6 +12,7 @@ cd ./AppImages
 
 set -- \
 	https://github.com/pkgforge-dev/Anylinux-AppImages/releases/download/demo/Qt6+dbus-demo-onlysoftware-$ARCH.AppImage
+	https://github.com/pkgforge-dev/Anylinux-AppImages/releases/download/demo/gtk4-demo-onlysoftware-$ARCH.AppImage
 
 for appimage do
 	wget "$appimage"
