b845d90df024335b8f5460abfc9a0bdb5ef35bd9
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-05-10 23:00:25 -0400

    test oolite

--- make-appimage.sh
+++ make-appimage.sh
@@ -17,7 +17,7 @@ mkdir -p ./AppImages
 cd ./AppImages
 
 set -- \
-	https://github.com/pkgforge-dev/CollaboraOffice-AppImage/releases/download/25.04.9.2-1%402026-05-01_1777639060/Collabora_Office-25.04.9.2-1-anylinux-"$ARCH".AppImage
+	https://github.com/mcarans/oolite/releases/download/1.93.0.7825-260511-0b5e81b/oolite-1.93.0.7825-260511-0b5e81b-"$ARCH".AppImage
 
 for appimage do
 	wget "$appimage"
