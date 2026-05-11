5df9135a9a6f2949a3dc82fa22b6f61272b28cfa
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-05-10 20:10:37 -0400

    list size

--- make-appimage.sh
+++ make-appimage.sh
@@ -58,7 +58,7 @@ for appimage do
 	count=0
 	while [ "$count" -lt 3 ]; do
 		echo 3 > /proc/sys/vm/drop_caches
-		echo "TESTING: $appimage"
+		echo "TESTING: $appimage - $(du -h "$appimage")"
 		bench-launch "$appimage"
 		count=$(( count + 1 ))
 		echo "===================="
